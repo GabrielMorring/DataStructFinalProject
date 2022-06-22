bab46ddfdbd5e357c74a751213ee94cac6af21b1
Gabriel Morring <gmorring7@gmail.com>
2022-06-22 13:53:03 -0700

    queue example finished

--- python/queue-example.py
+++ python/queue-example.py
@@ -1,3 +1,60 @@
 
-queue = []
+'''
+I have a hard time focusing on one thing at a time so i made a
+simple to do program where you enter all the things that you 
+have to do and it tells you them one at a time to help me focus
+on just that thing. 
+
+'''
+
+
+
+
+to_do = []
+
+print()
+print('To do app')
+print()
+
+print('Instructions: Enter all the tasks you want to complete \nthen do the tasks when that the program tells you to do.')
+
+print()
+print("Enter your tasks:")
+print()
+
+ans = '0'
+
+while ans != '3':
+
+    print('0. Enter task')
+    print('1. Do Task')
+    print('3: quit')
+
+    ans = input(': ')
+
+    if ans == '0':
+        to_do.append(input('Task: '))
+    elif ans == '1':
+
+        if len(to_do) != 0:
+            print()
+            print(to_do.pop(0))
+            print()
+            input('press enter to continue:')
+
+        else:
+            print()
+            print('You don\'t have any tasks to complete')
+            print()
+    
+    elif ans == '3':
+        print("Goodbye")
+    
+    else: 
+        print('You must enter one of the numbers.')    
+
+
+
+
+
 
